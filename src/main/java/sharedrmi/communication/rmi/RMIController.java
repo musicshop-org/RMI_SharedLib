b31b2d5f3c7fa851ba89d9694ca221146396e3f0
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-01 22:59:14 +0200

    RMIController Interface: Methods getRoles and getUsername

--- src/main/java/sharedrmi/communication/rmi/RMIController.java
+++ src/main/java/sharedrmi/communication/rmi/RMIController.java
@@ -3,8 +3,11 @@ package sharedrmi.communication.rmi;
 import sharedrmi.application.api.ProductService;
 import sharedrmi.application.api.ShoppingCartService;
 
+import javax.management.relation.Role;
 import java.rmi.Remote;
+import java.util.List;
 
 public interface RMIController extends ProductService, ShoppingCartService, Remote {
-
+    List<Role> getRoles();
+    String getUsername();
 }

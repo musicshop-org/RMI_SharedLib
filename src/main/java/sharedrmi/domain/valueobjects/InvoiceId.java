d09513782a2d8102d232b2e6246bb86c5d39c67a
schlaumeierisch <matthias.meier97@hotmail.com>
2022-04-02 10:54:57 +0200

    InvoiceId added

--- src/main/java/sharedrmi/domain/valueobjects/InvoiceId.java
+++ src/main/java/sharedrmi/domain/valueobjects/InvoiceId.java
@@ -1,4 +1,18 @@
 package sharedrmi.domain.valueobjects;
 
-public class InvoiceId {
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Getter
+public class InvoiceId implements Serializable {
+
+    private final long id;
+
+    public InvoiceId() {
+        this.id = LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond();
+    }
+
 }

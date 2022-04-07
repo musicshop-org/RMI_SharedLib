dd2459a3be9ab6d8a9c0a3098716af1fd36674b0
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-07 22:47:43 +0200

    added NoPermissionException

--- src/main/java/sharedrmi/application/api/ShoppingCartService.java
+++ src/main/java/sharedrmi/application/api/ShoppingCartService.java
@@ -4,19 +4,20 @@ import sharedrmi.application.dto.AlbumDTO;
 import sharedrmi.application.dto.CartLineItemDTO;
 import sharedrmi.application.dto.ShoppingCartDTO;
 
+import javax.naming.NoPermissionException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ShoppingCartService extends Remote{
 
-    ShoppingCartDTO getCart() throws RemoteException;
+    ShoppingCartDTO getCart() throws RemoteException, NoPermissionException;
 
-    void addProductToCart(AlbumDTO album, int amount) throws RemoteException;
+    void addProductToCart(AlbumDTO album, int amount) throws RemoteException, NoPermissionException;
 
-    void changeQuantity(CartLineItemDTO cartLineItemDTO, int quantity) throws RemoteException;
+    void changeQuantity(CartLineItemDTO cartLineItemDTO, int quantity) throws RemoteException, NoPermissionException;
 
-    void removeProductFromCart(CartLineItemDTO cartLineItemDTO) throws RemoteException;
+    void removeProductFromCart(CartLineItemDTO cartLineItemDTO) throws RemoteException, NoPermissionException;
 
-    void clearCart() throws RemoteException;
+    void clearCart() throws RemoteException, NoPermissionException;
 
 }

9752b11ec39b38792a9e23fa7992da1b3d843c31
schlaumeierisch <matthias.meier97@hotmail.com>
2022-04-08 17:37:58 +0200

    new lib

--- src/main/java/sharedrmi/application/api/InvoiceService.java
+++ src/main/java/sharedrmi/application/api/InvoiceService.java
@@ -5,11 +5,10 @@ import sharedrmi.domain.valueobjects.InvoiceId;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.Optional;
 
 public interface InvoiceService extends Remote {
 
-    Optional<InvoiceDTO> findInvoiceById(InvoiceId invoiceId) throws RemoteException;
+    InvoiceDTO findInvoiceById(InvoiceId invoiceId) throws RemoteException;
 
     void createInvoice(InvoiceDTO invoiceDTO) throws RemoteException;
 

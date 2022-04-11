0cebe50c0feaa2ffb4440a770f35017583b12ec3
fabian_egartner <fabian.egartner@hotmail.com>
2022-04-11 22:04:05 +0200

    added decreaseStockOfAlbum to ProductService

--- src/main/java/sharedrmi/application/api/ProductService.java
+++ src/main/java/sharedrmi/application/api/ProductService.java
@@ -3,6 +3,7 @@ package sharedrmi.application.api;
 import sharedrmi.application.dto.AlbumDTO;
 import sharedrmi.application.dto.ArtistDTO;
 import sharedrmi.application.dto.SongDTO;
+import sharedrmi.application.exceptions.AlbumNotFoundException;
 import sharedrmi.domain.enums.MediumType;
 
 import java.rmi.Remote;
@@ -13,10 +14,11 @@ public interface ProductService extends Remote{
 
     List<AlbumDTO> findAlbumsBySongTitle(String title) throws RemoteException;
 
-    AlbumDTO findAlbumByAlbumTitleAndMedium(String title, MediumType mediumType) throws RemoteException;
+    AlbumDTO findAlbumByAlbumTitleAndMedium(String title, MediumType mediumType) throws RemoteException, AlbumNotFoundException;
 
     List<SongDTO> findSongsByTitle(String title) throws RemoteException;
 
     List<ArtistDTO> findArtistsByName(String name) throws RemoteException;
 
+    void decreaseStockOfAlbum(String title, MediumType mediumType);
 }

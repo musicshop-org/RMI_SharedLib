03432a5a24ba52e6b830c241bb4c3455beae3b64
fabian_egartner <fabian.egartner@hotmail.com>
2022-04-11 19:48:37 +0200

    added findAlbumsByAlbumTitleAndMedium to ProductService

--- src/main/java/sharedrmi/application/api/ProductService.java
+++ src/main/java/sharedrmi/application/api/ProductService.java
@@ -3,6 +3,7 @@ package sharedrmi.application.api;
 import sharedrmi.application.dto.AlbumDTO;
 import sharedrmi.application.dto.ArtistDTO;
 import sharedrmi.application.dto.SongDTO;
+import sharedrmi.domain.enums.MediumType;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -12,7 +13,7 @@ public interface ProductService extends Remote{
 
     List<AlbumDTO> findAlbumsBySongTitle(String title) throws RemoteException;
 
-    AlbumDTO findAlbumByAlbumTitle(String title) throws RemoteException;
+    AlbumDTO findAlbumByAlbumTitleAndMedium(String title, MediumType mediumType) throws RemoteException;
 
     List<SongDTO> findSongsByTitle(String title) throws RemoteException;
 

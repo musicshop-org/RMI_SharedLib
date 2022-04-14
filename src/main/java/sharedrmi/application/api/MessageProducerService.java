9253a1d963fbb5f924698df462147339e589d097
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-14 14:11:50 +0200

    added MessageProducerService added MessageConsumerService

--- src/main/java/sharedrmi/application/api/MessageProducerService.java
+++ src/main/java/sharedrmi/application/api/MessageProducerService.java
@@ -1,11 +1,12 @@
 package sharedrmi.application.api;
 
 import javax.jms.JMSException;
+import javax.naming.NoPermissionException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
 public interface MessageProducerService extends Remote {
 
-    void publish(List<String> topics , String title, String messageText) throws RemoteException, JMSException;
+    void publish(List<String> topics , String title, String messageText) throws RemoteException, JMSException, NoPermissionException;
 }

ea4a9a04bd45d3baf3c5da01315f06b85a067323
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-17 12:23:46 +0200

    added UserService to RMIController

--- src/main/java/sharedrmi/communication/rmi/RMIController.java
+++ src/main/java/sharedrmi/communication/rmi/RMIController.java
@@ -7,7 +7,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
-public interface RMIController extends ProductService, ShoppingCartService, InvoiceService, CustomerService, MessageProducerService, Remote {
+public interface RMIController extends ProductService, ShoppingCartService, InvoiceService, CustomerService, MessageProducerService, UserService, Remote {
     List<Role> getRoles() throws RemoteException;
     String getUsername() throws RemoteException;
 }

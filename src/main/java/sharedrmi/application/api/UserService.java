66c6f5aa7ca3f0b87905322a64b4d149bbdd7fff
fabian_egartner <fabian.egartner@hotmail.com>
2022-04-22 21:39:22 +0200

    - added subscribe & unsubscribe methods to UserService - deleted MessageConsumerService

--- src/main/java/sharedrmi/application/api/UserService.java
+++ src/main/java/sharedrmi/application/api/UserService.java
@@ -9,4 +9,8 @@ public interface UserService extends Remote {
     List<String> getAllTopics() throws RemoteException;
 
     List<String> getSubscribedTopicsForUser(String username) throws RemoteException;
+
+    void subscribe(String topic) throws RemoteException;
+
+    void unsubscribe(String topic) throws RemoteException;
 }

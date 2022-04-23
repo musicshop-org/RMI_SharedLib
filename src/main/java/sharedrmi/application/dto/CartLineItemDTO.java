533c2f082e272c7b78b36143b084f88292a13f60
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-23 16:07:27 +0200

    added stock to CartLineItemDTO

--- src/main/java/sharedrmi/application/dto/CartLineItemDTO.java
+++ src/main/java/sharedrmi/application/dto/CartLineItemDTO.java
@@ -14,13 +14,15 @@ public class CartLineItemDTO implements Serializable {
     private final String name;
     private final int quantity;
     private final BigDecimal price;
+    private final int stock;
 
     @Builder
-    public CartLineItemDTO(MediumType mediumType, String name, int quantity, BigDecimal price) {
+    public CartLineItemDTO(MediumType mediumType, String name, int quantity, BigDecimal price, int stock) {
         this.mediumType = mediumType;
         this.name = name;
         this.quantity = quantity;
         this.price = price;
+        this.stock = stock;
     }
 
 }

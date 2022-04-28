ade15518633d2baaae55a1780b59354f2ad78d08
Benjamin Mayer <lolgame99@gmail.com>
2022-04-28 14:41:15 +0200

    changed rmicontroler interface to ejb

--- src/main/java/sharedrmi/communication/rmi/RMIController.java
+++ src/main/java/sharedrmi/communication/rmi/RMIController.java
@@ -3,11 +3,12 @@ package sharedrmi.communication.rmi;
 import sharedrmi.application.api.*;
 import sharedrmi.domain.valueobjects.Role;
 
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
+import javax.ejb.Remote;
 
-public interface RMIController extends ProductService, ShoppingCartService, InvoiceService, CustomerService, MessageProducerService, UserService, Remote {
+@Remote
+public interface RMIController extends ProductService, ShoppingCartService, InvoiceService, CustomerService, MessageProducerService, UserService {
     List<Role> getRoles() throws RemoteException;
     String getUsername() throws RemoteException;
 }

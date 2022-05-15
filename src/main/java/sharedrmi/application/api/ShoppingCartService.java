7190f821b9bfe828b744f053a9a392982973a8fc
schlaumeierisch <matthias.meier97@hotmail.com>
2022-05-15 19:28:46 +0200

    new function (addSongsToCart); new lib

--- src/main/java/sharedrmi/application/api/ShoppingCartService.java
+++ src/main/java/sharedrmi/application/api/ShoppingCartService.java
@@ -3,10 +3,12 @@ package sharedrmi.application.api;
 import sharedrmi.application.dto.AlbumDTO;
 import sharedrmi.application.dto.CartLineItemDTO;
 import sharedrmi.application.dto.ShoppingCartDTO;
+import sharedrmi.application.dto.SongDTO;
 
 import javax.ejb.Remote;
 import javax.naming.NoPermissionException;
 import java.io.Serializable;
+import java.util.List;
 
 @Remote
 public interface ShoppingCartService extends Serializable {
@@ -14,6 +16,8 @@ public interface ShoppingCartService extends Serializable {
     ShoppingCartDTO getCart() throws NoPermissionException;
 
     void addProductToCart(AlbumDTO album, int amount) throws NoPermissionException;
+
+    void addSongsToCart(List<SongDTO> songs) throws NoPermissionException;
 
     void changeQuantity(CartLineItemDTO cartLineItemDTO, int quantity) throws NoPermissionException;
 
